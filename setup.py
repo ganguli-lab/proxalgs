9529a89381232c56723c3e1190cf5eadae12ff01
Niru Maheswaranathan <nirum@stanford.edu>
2015-02-10 20:07:44 -0800

    fancy setup.py :princess:

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
+import os
 from distutils.core import setup
 
+version = '0.1'
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.md')).read()
+except IOError:
+    README = ''
+
+install_requires = [i.strip() for i in open("requirements.txt").readlines()]
+
+tests_require = ['nose']
+
+docs_require = ['Sphinx']
+
 setup(name='proxalgs',
-      version='0.1',
+      version=version,
       description='Proximal algorithms in python',
       author='Niru Maheshwaranathan',
       author_email='nirum@stanford.edu',
-      url='https://github.com/ganguli-lab/proxalgs.git',
-      requires=[i.strip() for i in open("requirements.txt").readlines()],
-      long_description='''
-            Contains tools for running proximal algorithms to minimize an
-            objective consisting of a sum of multiple functions. Exposes
-            a bunch of useful proximal operators for incorporating common
-            regularization terms in certain proximal algorithms.
-            ''',
+      url='https://github.com/ganguli-lab/proxalgs',
+      install_requires=install_requires,
+      extras_require={
+          'testing': tests_require,
+          'docs': docs_require
+      },
+      keywords="",
+      license="MIT",
+      long_description=README,
       classifiers=[
           'Intended Audience :: Science/Research',
           'Operating System :: MacOS :: MacOS X',
