793631559688d171c3519c715003222255fb877c
Niru Maheswaranathan <nirum@stanford.edu>
2015-02-26 14:54:03 -0800

    added dev reqs to setup.py

--- setup.py
+++ setup.py
@@ -9,24 +9,22 @@ try:
 except IOError:
     README = ''
 
-install_requires = ['numpy', 'scipy', 'hyperopt', 'sktensor']
-
-tests_require = ['nose']
-
-docs_require = ['Sphinx']
-
 setup(name='proxalgs',
       version=version,
       description='Proximal algorithms in python',
+      long_description=README,
       author='Niru Maheshwaranathan',
       author_email='nirum@stanford.edu',
       url='https://github.com/ganguli-lab/proxalgs',
-      requires=install_requires,
       license='MIT',
-      long_description=README,
       classifiers=[
           'Intended Audience :: Science/Research',
           'Operating System :: MacOS :: MacOS X',
           'Topic :: Scientific/Engineering :: Information Analysis'],
-      packages=['proxalgs']
+      packages=['proxalgs'],
+      requires=install_requires,
+      extras_require={
+          'dev': ['sphinx', 'sphinx-rtd-theme'],
+          'test': ['nose']
+      },
 )
